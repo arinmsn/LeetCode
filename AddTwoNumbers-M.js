f47f02a5800f1da9075238e68c3c148915a9fbb6
arinmsn <arinmsn@gmail.com>
2019-09-17 20:06:36 -0700

    Problem is solved.

--- AddTwoNumbers-M.js
+++ AddTwoNumbers-M.js
@@ -13,3 +13,36 @@ Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.   
 
 */
+
+var addTwoNumbers = function(l1, l2) {
+  var carry = 0;
+  var result = new ListNode(-1);
+  var resHead = result;
+  while (l1 !== null || l2 !== null) {
+    var sum = carry;
+    if (l1 !== null) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2 !== null) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    var node = null;
+    if (sum >= 10) {
+      node = new ListNode(sum - 10);
+      carry = 1;
+    } else {
+      node = new ListNode(sum);
+      carry = 0;
+    }
+    result.next = node;
+    result = result.next;
+  }
+  if (carry === 1) {
+    result.next = new ListNode(1);
+  }
+  return resHead.next;
+};
