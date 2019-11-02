a5bcde59751a7f9ba5eec220ebd86cbc43070e9b
arinmsn <arinmsn@gmail.com>
2019-11-01 20:00:22 -0700

    Regular Expression Matching (Hard) Solved

--- RegularExpressionMatching-H.js
+++ RegularExpressionMatching-H.js
@@ -44,3 +44,47 @@ p = "mis*is*p*."
 Output: false
 
 */
+var isMatch = function(s, p) {
+  var regMap = [];
+  buildMap(regMap, p);
+  return matchReg(s, 0);
+
+  function buildMap(map, reg) {
+    for (var i = 0; i < reg.length; i++) {
+      if (reg[i] === "*") {
+        if (map.length >= 1) {
+          map[map.length - 1] = map[map.length - 1] + "*";
+        }
+      } else {
+        map.push(reg[i]);
+      }
+    }
+  }
+  function matchReg(str, index) {
+    if (index === regMap.length) {
+      return str === "" ? true : false;
+    }
+
+    //single char
+    if (regMap[index].indexOf("*") === -1) {
+      if (str[0] === regMap[index] || (regMap[index] === "." && str != "")) {
+        return matchReg(str.substring(1, str.length), index + 1);
+      } else {
+        return false;
+      }
+    }
+
+    //.*  a*
+    while (
+      str !== "" &&
+      (str[0] === regMap[index][0] || regMap[index][0] === ".")
+    ) {
+      if (matchReg(str, index + 1)) {
+        return true;
+      }
+      str = str.substring(1, str.length);
+    }
+
+    return matchReg(str, index + 1);
+  }
+};
